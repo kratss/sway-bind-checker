111a4c2167fd205cb906846602561882ea17d213
krats <krats@noreply.codeberg.org>
2024-07-26 21:39:40 +0200

    Improve output readability and error handling

--- sway-bind-checker.fish
+++ sway-bind-checker.fish
@@ -1,57 +1,70 @@
 #!/usr/bin/env fish
-# Indicate used and unused shortcuts in sway
+# Read used and unused bindings from sway config 
+# Usage: Pass any sway config file to see a list of available bindings
+# TODO: Detect key bindings set in variables. E.g. default config sets j
+# to $left and uses $left in bindings
 
-function keycheck
-set -g keys_unbound $keys
-set i 0
-for key in $keys
-    set i (math $i + 1)
-    if string match --quiet --entire -- $key $argv
-        set keys_unbound[$i] ""
-    end
+argparse "h/help" -- $argv
+if test -n "$_flag_help"
+    echo "Usage: Pass any sway config file to see a list of available bindings"
+    return
+else if test -z "$argv"
+    echo "Error: Requires sway config file as argument"
+    return
 end
-set i 1
-for key in $keys_unbound
-    if test -z "$key"
-        set -e keys_unbound[$i]
-    else
-        set i (math $i +1)
+
+function keycheck  
+    set -g keys_unbound $keys
+    set i 0
+    for key in $keys
+        set i (math $i + 1)
+        if string match --quiet --entire -- $key $argv
+            set keys_unbound[$i] ""
+        end
     end
-end
+    set i 1
+    for key in $keys_unbound
+        if test -z "$key"
+            set -e keys_unbound[$i]
+        else
+            set i (math $i +1)
+        end
+    end
 end
 
 set conf $(cat $argv)
 set full_line (string match -e -r 'bindsym*' $conf)
-set command (string match -r 'bindsym\s[$a-zA-Z0-9-+]*' $conf)
-set command (string replace 'bindsym ' ''  $command)
-set command_mod (string match -- '*mod*' $command)
+set cmnd (string match -r 'bindsym\s[$a-zA-Z0-9-+]*' $conf)
+set cmnd (string replace 'bindsym ' ''  $cmnd)
+set cmnd_mod (string replace -f -r -- '\$mod\+' '' $cmnd)
+set cmnd_mod_nill (string match -v -- '*Shift*' $cmnd_mod | \
+    string match -v -- '*Alt*')
+set cmnd_mod_shift (string replace -f -r -- 'Shift\+' '' $cmnd_mod | \
+    string match --invert -- '*Alt*')
+set cmnd_mod_alt (string replace -f -r -- 'Alt\+' '' $cmnd_mod | \
+    string match --invert -- '*Shift*')
 
-set command_mod (string replace -r -- '\$mod\+' '' $command)
-set command_mod_nill (string match --invert -- '*Shift*' $command_mod)
-set command_mod_shift (string match -- '*Shift*' $command_mod)
-set command_mod_shift (string replace -r -- 'Shift\+' '' $command_mod_shift)
-set command_mod_alt (string match -- '*Alt*' $command_mod)
-set command_mod_alt (string replace -r -- 'Alt\+' '' $command_mod_alt)
-set command_ctrl 
-set command_ctrl_shift
-
-#todo: get % to work
 set keys a b c d e f g h i j k l m n o p q r s t u v w x y z\
     "`" 1 2 3 4 5 6 7 8 9 0 "-" "+" \
-    "~" "!" "@" "#" "\$" "^" "&" \* \( \) "_" "+" \
+    "~" "!" "@" "#" "%" "\$" "^" "&" \* \( \) "_" "+" \
     \; \' ":" \" \
     "," "." "/" \
     "<" ">" "?"
 
-
-
-keycheck $command_mod_nill
-printf "\nUnbound \$mod: $keys_unbound"
-
-keycheck $command_mod_shift
-printf "\nunbound \$mod+Shift: $keys_unbound"
-
-keycheck $command_mod_alt
-printf "\nunbound \$mod+Alt: $keys_unbound"
-
+printf '\nUnbound keys:\n'
+keycheck $cmnd_mod_nill
+printf '$mod+%s\n' $keys_unbound | column
+printf '\n'
+keycheck $cmnd_mod_shift
+printf '$mod+Shift+%s\n' $keys_unbound | column
+printf '\n'
+keycheck $cmnd_mod_alt
+printf '$mod+Alt+%s\n' $keys_unbound | column
+printf '\n'
+echo "Currently bound keys: "
+printf '$mod+%s\n' $cmnd_mod_nill | column
+printf '\n'
+printf '$mod+Shift+%s\n' $cmnd_mod_shift | column
+printf '\n'
+printf '$mod+Alt+%s\n' $cmnd_mod_alt | column
 
